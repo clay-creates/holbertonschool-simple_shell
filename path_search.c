00878c99fd5c1c8a8927f3883dd0a7007675269a
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-01 16:29:37 +0000

    indentation and connect path_search to user_input

--- path_search.c
+++ path_search.c
@@ -4,51 +4,51 @@
 
 void path_search(const char *executable_name)
 {
-    char *path = getenv("PATH");
-    char *path_token = strtok(path, ":");
+	char *path = getenv("PATH");
+	char *path_token = strtok(path, ":");
 
-    while (path_token != NULL)
-    {
-        char *executable_path = malloc(strlen(path_token) + strlen(executable_name) + 2);
-        if (executable_path == NULL)
-        {
-            perror("Memory allocation failed.");
-            exit(1);
-        }
-        strcpy(executable_path, path_token);
-        strcat(executable_path, "/");
-        strcat(executable_path, executable_name);
-        if (access(executable_path, F_OK) == 0)
-        {
-            printf("Found executable at %s\n", executable_path); /** need to execute if found */
+	while (path_token != NULL)
+	{
+		char *executable_path = malloc(strlen(path_token) + strlen(executable_name) + 2);
+		if (executable_path == NULL)
+		{
+			perror("Memory allocation failed.");
+			exit(1);
+		}
+		strcpy(executable_path, path_token);
+		strcat(executable_path, "/");
+		strcat(executable_path, executable_name);
+		if (access(executable_path, F_OK) == 0)
+		{
+			printf("Found executable at %s\n", executable_path); /** need to execute if found */
 
-            __pid_t pid = fork();
-            if (pid < 0)
-            {
-                perror("Fork failed.");
-                exit(1);
-            }
+			__pid_t pid = fork();
+			if (pid < 0)
+			{
+				perror("Fork failed.");
+				exit(1);
+			}
 
-            if (pid == 0)
-            {
-                char *args[] = {executable_path, NULL};
-                execve(executable_path, args, NULL);
+			if (pid == 0)
+			{
+				char *args[] = {executable_path, NULL};
+				execve(executable_path, args, NULL);
 
-                perror("Execve failed.");
-                exit(1);
-            }
+				perror("Execve failed.");
+				exit(1);
+			}
 
-            if (pid > 0)
-            {
-                int status;
-                wait(&status);
-                if (WIFEEXITED(status))
-                {
-                    printf("Child process exited with status %d\n", WEXITSTATUS(status));
-                }
-            }
-        }
-        free(executable_path);
-        path_token = strtok(NULL, ":");
-    }
+			if (pid > 0)
+			{
+				int status;
+				wait(&status);
+				if (WIFEEXITED(status))
+				{
+					printf("Child process exited with status %d\n", WEXITSTATUS(status));
+				}
+			}
+		}
+		free(executable_path);
+		path_token = strtok(NULL, ":");
+	}
 }
--- user_input.c
+++ user_input.c
@@ -2,48 +2,56 @@
 
 /***/
 
-char *read_line(void) {
-  size_t buffsize = 1024;
-  char *buffer = malloc(buffsize * sizeof(char));
-  if (buffer == NULL) {
-	perror("Buffer allocation failed.");
-	exit(1);
-  }
-  printf("$: ");
-  int characters = getline(&buffer, &buffsize, stdin);
-  if (characters == -1) {
-	perror("Error reading line");
-	exit(1);
-  }
-  return buffer;
+char *read_line(void)
+{
+	size_t buffsize = 1024;
+	char *buffer = malloc(buffsize * sizeof(char));
+	if (buffer == NULL)
+	{
+		perror("Buffer allocation failed.");
+		exit(1);
+	}
+	printf("$: ");
+	int characters = getline(&buffer, &buffsize, stdin);
+	if (characters == -1)
+	{
+		perror("Error reading line");
+		exit(1);
+	}
+	return buffer;
 }
 
-char *tokenize_line(char *buffer) {
-  char *token = strtok(buffer, " \t\n\r");
-  if (token == NULL) {
-	perror("Error tokenizing line");
-	exit(1);
-  }
-  return token;
+char *tokenize_line(char *buffer)
+{
+	char *token = strtok(buffer, " \t\n\r");
+	if (token == NULL)
+	{
+		perror("Error tokenizing line");
+		exit(1);
+	}
+	return token;
 }
 
-void print_words(char *token) {
-  while (token != NULL) {
-	printf("%s\n", token);
-	token = strtok(NULL, " \t\n\r");
-  }
+void print_words(char *token)
+{
+	while (token != NULL)
+	{
+		printf("%s\n", token);
+		token = strtok(NULL, " \t\n\r");
+	}
 }
 
-int main(void) {
-  while (!feof(stdin)) {
-	char *buffer = read_line();
-	char *token = tokenize_line(buffer);
-	print_words(token);
-	free(buffer);
-  }
-  return 0;
+int main(void)
+{
+	while (!feof(stdin))
+	{
+		char *buffer = read_line();
+		char *executable_name = tokenize_line(buffer);
+		path_search(executable_name);
+		free(buffer);
+	}
+	return 0;
 }
-
 
 /**int main(void)
 {
