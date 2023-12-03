4e81245ea09eb3ac230be71a502fbc5b6b116fc3
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-03 00:41:22 +0000

    make tokenize_line file

---
+++ tokenize_line.c
@@ -0,0 +1,19 @@
+/**
+ * tokenize_line - uses strtok to create tokens from user input (saved in buffer)
+ * @buffer: array holding user input (from read_line)
+ *
+ * Return: array of tokens created from user input
+ */
+
+char **tokenize_line(char *buffer)
+{
+	char **tokens = malloc(2 * sizeof(char *));
+	tokens[0] = strtok(buffer, " \t\n\r");
+	tokens[1] = strtok(NULL, " \t\n\r");
+	if (tokens == NULL)
+	{
+		perror("Error tokenizing line");
+		exit(1);
+	}
+	return tokens;
+}
