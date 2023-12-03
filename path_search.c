123679d785a2beeddb2349b14040d76463efa747
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-03 00:42:51 +0000

    update path search file

--- path_search.c
+++ path_search.c
@@ -1,21 +1,26 @@
 #include "main.h"
 
-/***/
+/**
+ * path_search - tokenizes copy of PATH to search for executable, given by user
+ * @executable_name: name of command entered into user input (tokens[0])
+ * @args: arguments for executable (found in tokens[1])
+ */
 
-void path_search(const char *executable_name)
+void path_search(const char *executable_name, char *args)
 {
 	char *path = getenv("PATH");
-	char *path_token = strtok(path, ":");
+	char *path_copy = strdup(path);
+	char *dir = strtok(path_copy, ":");
 
-	while (path_token != NULL)
+	while (dir != NULL)
 	{
-		char *executable_path = malloc(strlen(path_token) + strlen(executable_name) + 2);
+		char *executable_path = malloc(strlen(path_copy) + strlen(executable_name) + 2);
 		if (executable_path == NULL)
 		{
 			perror("Memory allocation failed.");
 			exit(1);
 		}
-		strcpy(executable_path, path_token);
+		strcpy(executable_path, path_copy);
 		strcat(executable_path, "/");
 		strcat(executable_path, executable_name);
 		if (access(executable_path, F_OK) == 0)
@@ -31,8 +36,8 @@ void path_search(const char *executable_name)
 
 			if (pid == 0)
 			{
-				char *args[] = {executable_path, NULL};
-				execve(executable_path, args, NULL);
+				char *exec_args[] = {executable_path, args, NULL};
+				execve(executable_path, exec_args, NULL);
 
 				perror("Execve failed.");
 				exit(1);
@@ -48,7 +53,12 @@ void path_search(const char *executable_name)
 				}
 			}
 		}
+		else
+		{
+			printf("Command not found: %s\n", executable_name);
+		}
 		free(executable_path);
-		path_token = strtok(NULL, ":");
+		dir = strtok(NULL, ":");
 	}
+	free(path_copy);
 }
