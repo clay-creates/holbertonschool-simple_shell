7b16f684f732dde5f679c608d00f6ad184f70e3d
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-04 14:15:31 +0000

    add basename var

--- path_search.c
+++ path_search.c
@@ -15,16 +15,17 @@ void path_search(const char *executable_name, char *args)
 
 	while (dir != NULL)
 	{
+		char *base_name = basename((char *)executable_name);
 		char *executable_path = malloc(strlen(path_copy) + strlen(executable_name) + 2);
 		if (executable_path == NULL)
 		{
 			perror("Memory allocation failed.");
 			exit(1);
 		}
-		/**strcpy(executable_path, path_copy);
+		strcpy(executable_path, dir);
 		strcat(executable_path, "/");
-		strcat(executable_path, executable_name);
-		if (access(executable_path, F_OK) == 0)*/
+		strcat(executable_path, base_name);
+		if (access(executable_path, F_OK) == 0)
 		{
 			printf("Found executable at %s\n", executable_path); /** need to execute if found */
 
