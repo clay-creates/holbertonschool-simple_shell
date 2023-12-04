e8fdb1126a043faa2cd927e16cbdcafaa039a604
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-04 13:58:06 +0000

    fix mixed declaration

--- read_line.c
+++ read_line.c
@@ -10,13 +10,15 @@ char *read_line(void)
 {
 	size_t buffsize = 1024;
 	char *buffer = malloc(buffsize * sizeof(char));
+	int characters;
+
 	if (buffer == NULL)
 	{
 		perror("Buffer allocation failed.");
 		exit(1);
 	}
 	printf("$: ");
-	int characters = getline(&buffer, &buffsize, stdin);
+	characters = getline(&buffer, &buffsize, stdin);
 	if (characters == -1)
 	{
 		perror("Error reading line");
