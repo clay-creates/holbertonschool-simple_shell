8f9e40466f1e65f7f3faf6cb7878728f602220bb
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-04 13:58:36 +0000

    fixed mixed declaration

--- path_search.c
+++ path_search.c
@@ -11,6 +11,7 @@ void path_search(const char *executable_name, char *args)
 	char *path = getenv("PATH");
 	char *path_copy = strdup(path);
 	char *dir = strtok(path_copy, ":");
+	__pid_t pid;
 
 	while (dir != NULL)
 	{
@@ -27,7 +28,7 @@ void path_search(const char *executable_name, char *args)
 		{
 			printf("Found executable at %s\n", executable_path); /** need to execute if found */
 
-			__pid_t pid = fork();
+			pid = fork();
 			if (pid < 0)
 			{
 				perror("Fork failed.");
@@ -58,6 +59,7 @@ void path_search(const char *executable_name, char *args)
 			printf("Command not found: %s\n", executable_name);
 		}
 		free(executable_path);
+		free(args);
 		dir = strtok(NULL, ":");
 	}
 	free(path_copy);
